8781fb85579f7b3d428354af1cdf452f4b873cea
Samikshya Timalsina <hce076bct034@hcoe.edu.np>
2024-05-13 17:46:51 +0545

    model added

--- src/com/model/Book.java
+++ src/com/model/Book.java
@@ -1,5 +1,39 @@
 package com.model;
 
 public class Book {
-
+	private String name;
+	private String author;
+	private int edition;
+	private String publication;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getEdition() {
+		return edition;
+	}
+	public void setEdition(int edition) {
+		this.edition = edition;
+	}
+	public String getPublication() {
+		return publication;
+	}
+	public void setPublication(String publication) {
+		this.publication = publication;
+	}
+	@Override
+	public String toString() {
+		return "Book [name=" + name + ", author=" + author + ", edition=" + edition + ", publication=" + publication
+				+ "]";
+	}
+	
+	
 }
